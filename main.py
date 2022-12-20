1c08c92245fe24691acceed1092021100ecdfd52
Ivan Ivanov <101635841+vin-57@users.noreply.github.com>
2022-12-20 21:17:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 import cv2
 import numpy as np
-from pyzbar.pyzbar import decode # libary, that allows us to detect qr-code and decod it
-
+from pyzbar.pyzbar import decode
+ 
 #img = cv2.imread('1.png')
-#code=decode(img)
-#print(code)
-
-cap=cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
-
-with open('myDataFile.txt') as f:
+ 
+with open('myDataFile.text') as f:
     myDataList = f.read().splitlines()
- #print(myDataList)
-
+ 
 while True:
-
+ 
     success, img = cap.read()
-    for barcode in deode (img):
-    #print (barcode.data)
-    myData = barcode.data.decode('utf-8')
-    print(myData)
-    
-    if myData in myDataList:
-        myOutput='Inventoryed'
-        myColor = (0, 255, 0)
-    else:
-        myOutput='Un-Inventoryed'
-        myColor = (0, 0, 255)
-
-    pts = np.array([barcode.polygon],np.int32) #use poligon
-    pts = pts.reshape ((-1, 1,2))
-    cv2.polylines(img, [pts], True, myColor, 5)
-    pts2 = barcode.rect
-    cv2.putText(img, myData, (pts2[0], pts2[1], cv2.FONT_HERSHEY_SIMPEX, 0.9, myColor, 2))
-
-cv2.imshow("result", img)
-cv2.waitKey(1)
+    for barcode in decode(img):
+        myData = barcode.data.decode('utf-8')
+        print(myData)
+ 
+        if myData in myDataList:
+            myOutput = 'Authorized'
+            myColor = (0,255,0)
+        else:
+            myOutput = 'Un-Authorized'
+            myColor = (0, 0, 255)
+ 
+        pts = np.array([barcode.polygon],np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img,[pts],True,myColor,5)
+        pts2 = barcode.rect
+        cv2.putText(img,myOutput,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,myColor,2)
+ 
+    cv2.imshow('Result',img)
+    cv2.waitKey(1)
